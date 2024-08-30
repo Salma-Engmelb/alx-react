c5d276f06ffa509104a07a4d6ba0c6e1f26da4ca
Salma-Engmelb <docmel2409@gmail.com>
2024-08-30 23:24:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './holberton_logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
       </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="fname">Email:</label>
+        <input type="email" id="email" />
+        <label htmlFor="lname">Password:</label>
+        <input type="password" id="password" />
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
